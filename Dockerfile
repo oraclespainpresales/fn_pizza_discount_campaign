f7ebb1ee2a695506d173cd418f89bf4a5f56e0de
test_oke <ivan.sampedro@oracle.com>
2020-01-12 01:53:40 -0800

    first mayor version ATP dto campaign

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,25 @@ FROM openjdk:13 as build-stage
 WORKDIR /function
 RUN curl https://www-eu.apache.org/dist/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz -o apache-maven-3.6.3-bin.tar.gz 
 RUN tar -zxvf apache-maven-3.6.3-bin.tar.gz
+RUN yum install -y unzip
+
+COPY dbwallet.zip /function
+RUN unzip /function/dbwallet.zip -d /function/wallet/ && rm /function/dbwallet.zip
 
 ENV PATH="/function/apache-maven-3.6.3/bin:${PATH}"
 
 ENV MAVEN_OPTS -Dhttp.proxyHost= -Dhttp.proxyPort= -Dhttps.proxyHost= -Dhttps.proxyPort= -Dhttp.nonProxyHosts= -Dmaven.repo.local=/usr/share/maven/ref/repository
 ADD pom.xml /function/pom.xml
 ADD src /function/src
+ADD libs/* /function/target/libs/
+
+RUN ["mvn", "install:install-file", "-Dfile=/function/target/libs/ojdbc8.jar",    "-DgroupId=com.oracle.jdbc", "-DartifactId=ojdbc8",    "-Dversion=18.3.0.0", "-Dpackaging=jar"]
+RUN ["mvn", "install:install-file", "-Dfile=/function/target/libs/ucp.jar",       "-DgroupId=com.oracle.jdbc", "-DartifactId=ucp",       "-Dversion=18.3.0.0", "-Dpackaging=jar"]
+RUN ["mvn", "install:install-file", "-Dfile=/function/target/libs/oraclepki.jar", "-DgroupId=com.oracle.jdbc", "-DartifactId=oraclepki", "-Dversion=18.3.0.0", "-Dpackaging=jar"]
+RUN ["mvn", "install:install-file", "-Dfile=/function/target/libs/osdt_core.jar", "-DgroupId=com.oracle.jdbc", "-DartifactId=osdt_core", "-Dversion=18.3.0.0", "-Dpackaging=jar"]
+RUN ["mvn", "install:install-file", "-Dfile=/function/target/libs/osdt_cert.jar", "-DgroupId=com.oracle.jdbc", "-DartifactId=osdt_cert", "-Dversion=18.3.0.0", "-Dpackaging=jar"]
+
+#RUN ["mvn", "package"]
 
 RUN ["mvn", "package", \
     "dependency:copy-dependencies", \
@@ -15,13 +28,15 @@ RUN ["mvn", "package", \
     "-Dmdep.prependGroupId=true", \
     "-DoutputDirectory=target","-e" ]
 
-RUN /usr/java/openjdk-13/bin/jlink --no-header-files --no-man-pages --strip-java-debug-attributes --output /function/fnjre --add-modules $(/usr/java/openjdk-13/bin/jdeps --print-module-deps /function/target/function.jar)
+#RUN /usr/java/openjdk-13/bin/jdeps --print-module-deps --class-path '/function/target/*' /function/target/function.jar
+RUN /usr/java/openjdk-13/bin/jlink --no-header-files --no-man-pages --strip-java-debug-attributes --output /function/fnjre --add-modules $(/usr/java/openjdk-13/bin/jdeps --ignore-missing-deps --print-module-deps --class-path '/function/target/*' /function/target/function.jar)
 
 FROM oraclelinux:8-slim
 WORKDIR /function
 
 COPY --from=build-stage /function/target/*.jar /function/
 COPY --from=build-stage /function/fnjre/ /function/fnjre/
+COPY --from=build-stage /function/wallet/ /function/wallet/
 COPY libfnunixsocket.so /lib
 
 ENTRYPOINT [ "/function/fnjre/bin/java", \
@@ -29,4 +44,6 @@ ENTRYPOINT [ "/function/fnjre/bin/java", \
     "-cp", "/function/*", \
     "com.fnproject.fn.runtime.EntryPoint" ]
 
-CMD ["com.example.fn.HelloFunction::handleRequest"]
+#ENTRYPOINT [ "/usr/bin/java","-XX:+UseSerialGC","--enable-preview","-Xshare:on","-cp", "/function/*","com.fnproject.fn.runtime.EntryPoint" ]
+
+CMD ["com.example.fn.GetDiscount::handleRequest"]
---
+++ src/main/java/com/example/fn/GetDiscount.java
@@ -0,0 +1,130 @@
+package com.example.fn;
+
+//import com.cedarsoftware.util.io.JsonWriter;
+import java.sql.*;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+public class GetDiscount {
+
+    public static class Input {
+        public String demozone;
+        public String paymentMethod;
+        public String pizzaPrice;
+
+        public String toString() {
+            StringBuilder stb = new StringBuilder("{");
+            stb.append("'demozone':'").append(demozone).append("'");
+            stb.append("'paymentMethod':'").append(paymentMethod).append("'");
+            stb.append("'pizzaPrice':'").append(pizzaPrice).append("'");
+            stb.append("}");
+            return stb.toString();
+        }
+    }
+
+    public String handleRequest(Input pizzaData) {
+        String exitValues    = "SALIDA::";
+        ResultSet resultSet  = null;
+        Connection con       = null;
+        float discount       = 0;
+
+        try {
+            String paymentMethod = pizzaData.paymentMethod.toUpperCase();
+            String demozone      = pizzaData.demozone.toUpperCase();
+            String pizzaPrice    = pizzaData.pizzaPrice;
+
+            //cast string input into a float
+            System.err.println("inside Discount Function gigis fn function!!! ");
+            float totalPaidValue  = Float.parseFloat(pizzaPrice);
+
+            String dbUser         = System.getenv().get("DB_USER");
+            String dbPassword     = System.getenv().get("DB_PASSWORD");
+            String dbUrl          = System.getenv().get("DB_URL") + System.getenv().get("DB_SERVICE_NAME");
+            String clientCredPath = System.getenv().get("CLIENT_CREDENTIALS");
+            String keyStorePasswd = System.getenv().get("KEYSTORE_PASSWORD");
+            String truStorePasswd = System.getenv().get("TRUSTSTORE_PASSWORD");
+            
+            /*********** FOR TESTING ONLY *************************************
+            System.err.println("ENV::" + dbUser);
+            System.err.println("ENV::" + dbPassword);
+            System.err.println("ENV::" + dbUrl);
+            System.err.println("ENV::" + clientCredPath);            
+            System.err.println("ENV::" + keyStorePasswd);
+            System.err.println("ENV::" + truStorePasswd);       
+            */
+
+            System.setProperty("oracle.jdbc.driver.OracleDriver", "true");
+            System.setProperty("oracle.net.ssl_version", "1.2");
+            System.setProperty("javax.net.ssl.keyStore", "${clientCredPath}/keystore.jks");
+            System.setProperty("javax.net.ssl.keyStorePassword", keyStorePasswd);
+            System.setProperty("javax.net.ssl.trustStore", "${clientCredPath}/truststore.jks");
+            System.setProperty("javax.net.ssl.trustStorePassword", truStorePasswd);
+            System.setProperty("oracle.net.tns_admin", clientCredPath);
+
+            try {
+                DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
+                //System.err.println("QUERY:: Driver Registration [" + dbUrl +" "+ dbUser+" "+dbPassword + "]");
+                con = DriverManager.getConnection(dbUrl,dbUser,dbPassword);
+                if (con != null) {
+                    System.err.println("Connected to Oracle ATP DB successfully");                
+                    //System.err.println("QUERY:: Driver getConnection");
+
+                    StringBuilder stb = new StringBuilder("SELECT NVL (");
+                    stb.append("(SELECT SUM(discount) FROM campaign WHERE ");
+                    stb.append("demozone LIKE ? ");
+                    stb.append("AND paymentmethod LIKE ? ");
+                    stb.append("AND CURRENT_DATE BETWEEN date_ini AND date_end+1 ");
+                    stb.append("AND min_amount <= ?)");
+                    stb.append(",0) as DISCOUNT FROM DUAL");
+                    PreparedStatement pstmt = con.prepareStatement(stb.toString());
+
+                    pstmt.setString(1,demozone);
+                    pstmt.setString(2,paymentMethod);
+                    pstmt.setFloat(3,Float.parseFloat(pizzaPrice));
+                    
+                    /*********** FOR TESTING ONLY *************************************
+                    System.err.println("QUERY::    " + stb.toString());
+                    System.err.println("QUERY:: 1. " + demozone);
+                    System.err.println("QUERY:: 2. " + paymentMethod);
+                    System.err.println("QUERY:: 3. " + pizzaPrice);
+                    */
+                    
+                    System.err.println("[" + pizzaData.toString() + "] - Pizza Price before discount: " + totalPaidValue + "$");
+                    resultSet = pstmt.executeQuery();
+                    if (resultSet.next()){
+                        discount = Float.parseFloat(resultSet.getString("DISCOUNT"))/100;
+                        //apply calculation to float eg: discount = 10%
+                        System.err.println("[" + pizzaData.toString() + "] - discount: " + discount + "%");
+                    }
+                    else {
+                        System.err.println ("[" + pizzaData.toString() + "] - No Discount campaign for this payment!");
+                    }
+
+                    totalPaidValue -=  (totalPaidValue*discount);
+                    System.err.println("[" + pizzaData.toString() + "] - total Pizza Price after discount: " + totalPaidValue + "$");
+                    exitValues = Float.toString(totalPaidValue);
+                }
+                else {
+                    System.err.println("[" + pizzaData.toString() + "] - Error: DB Connection Problem"); 
+                    throw new NullPointerException("DB Connection Problem : NULL!");
+                }
+            }     
+            catch (Exception ex) {
+                StringWriter errors = new StringWriter();
+                ex.printStackTrace(new PrintWriter(errors));                 
+                exitValues = pizzaData.toString() + " - Error: " + ex.toString() + "\n" + ex.getMessage() + errors.toString();                
+            }  
+            finally {
+                con.close();
+            }
+        }
+        catch (Exception ex){
+            StringWriter errors = new StringWriter();
+            ex.printStackTrace(new PrintWriter(errors));                 
+            exitValues = pizzaData.toString() + " - Error: " + ex.toString() + "\n" + ex.getMessage() + errors.toString();;
+        }
+        finally{
+            return exitValues;
+        }
+    }
+}
